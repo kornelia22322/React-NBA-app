47a5ea036799a9fc99bbd9e25cf7a96a119d0932
korneliarohullko <korneliarohulko@gmail.com>
2018-03-02 03:27:29 +0100

    Add header, footer and some styling

---
+++ src/components/featured.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Featured = (props) => {
+    console.log(props.slides);
+    return(
+        <div>slider</div>
+    )
+}
+
+export default Featured;
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
+//components
+import Featured from './featured';
+
+const URL_HOME = 'http://localhost:3004/home'
+
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            home: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_HOME, {method: 'GET'})
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                home: json
+            });
+        })
+    }
+
     render() {
         return(
-            <div>Home</div>
+            <div>
+                <Featured slides = {this.state.home.slider}/>
+            </div>
         )
     }
 }
